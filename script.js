f64ca7689673f97e3e46ed0423dcaff4892d07ec
Arlaine Sanchez <amaesanchez@gmail.com>
2022-10-19 02:18:20 -0700

    append input text to newDiv

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ currImg.addEventListener("change", () => {
 })
 */
 
+const outputdiv = document.querySelector(".output");
+const currImg = document.querySelector('input[name="imagefile"]');
+const topText = document.querySelector('input[name="toptext"]');
+const bottomText = document.querySelector('input[name="bottomtext"]');
+const form = document.querySelector('#memeform');
+const deleteBtns = document.querySelectorAll('div button');
+
+
 function generateMeme(currImg, topText, bottomText) {
   // create new elements to append to output
   const newDiv = document.createElement('div');
   const newimg = document.createElement('img');
   const top = document.createElement('p');
   const bottom = document.createElement('p');
+  const btn = document.createElement('button')
 
-  //DEBUG, topText & bottomText are being read correctly
-  //but not showing on img
-  //does show when img tag is removed
   console.log(newDiv);
 
   // DEBUG, don't know how to extract input file
@@ -30,52 +36,24 @@ function generateMeme(currImg, topText, bottomText) {
   // change text in p
   top.innerText = topText;
   bottom.innerText = bottomText;
+  btn.innerText = "Delete";
 
   // append p to img & img to div -- load img
-  outputdiv.append(newDiv)
-  newDiv.append(newimg)
-  newimg.append(top, bottom)
-
+  outputdiv.append(newDiv);
+  newDiv.append(newimg, top, bottom, btn);
 }
 
-function clearInput() {
-    topText.value = '';
-    bottomText.value = '';
-}
-
-
-const outputdiv = document.querySelector(".output");
-const currImg = document.querySelector("#imagefile");
-const topText = document.querySelector("#toptext");
-const bottomText = document.querySelector("#bottomtext");
-const form = document.querySelector('#memeform');
-
-form.addEventListener("submit", function() {
+form.addEventListener("submit", function(e) {
   generateMeme(currImg, topText.value, bottomText.value);
-  clearInput()
+  topText.value = '';
+  bottomText.value = '';
 });
 
-// delete.addEventListener("click", deleteLastMeme)?
+for (let btn of deleteBtns) {
+  btn.addEventListener("click", function(e) {
+    e.target.parentElement.remove()
+  });
+}
 
 
-
-
-
-// ------ just testing stuff
-const meme = document.createElement("img");
-const newDiv = document.createElement('div');
-const topT = document.createElement('p');
-const bottomT = document.createElement('p');
-meme.setAttribute("src", "../Personal website/photos/me.jpeg")
-meme.classList.add('memebg')
-newDiv.classList.add('container');
-topT.classList.add('topcenter');
-bottomT.classList.add('bottomcenter');
-topT.innerText = "try";
-bottomT.innerText = "again";
-
-console.log(newDiv)
-
-outputdiv.append(newDiv)
-newDiv.append(meme)
-meme.append(bottomT, topT)
+// DEBUG, how to save img WITH text?
