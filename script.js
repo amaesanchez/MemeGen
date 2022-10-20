dac653a64724addddb0b7b8c77c4056f1ee35489
Arlaine Sanchez <amaesanchez@gmail.com>
2022-10-19 17:49:20 -0700

    create delete button, add image URL to JS

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-/*
-currImg.addEventListener("change", () => {
-  const imgDataURL = URL.createObjectURL(currImg.files[0]);
-  image = new Image();
-  image.src = imgDataURL;
-})
-*/
-
 const outputdiv = document.querySelector(".output");
 const currImg = document.querySelector('input[name="imagefile"]');
 const topText = document.querySelector('input[name="toptext"]');
 const bottomText = document.querySelector('input[name="bottomtext"]');
+const textSize = document.querySelector('#textsize');
 const form = document.querySelector('#memeform');
 const deleteBtns = document.querySelectorAll('div button');
 
 
-function generateMeme(currImg, topText, bottomText) {
+function generateMeme(url, fontsize, topText, bottomText) {
   // create new elements to append to output
   const newDiv = document.createElement('div');
   const newimg = document.createElement('img');
@@ -22,10 +15,8 @@ function generateMeme(currImg, topText, bottomText) {
   const bottom = document.createElement('p');
   const btn = document.createElement('button')
 
-  console.log(newDiv);
-
   // DEBUG, don't know how to extract input file
-  // img.setAttribute("src", "#");
+  newimg.src = url;
 
   // change classes to make meme
   newDiv.classList.add('container');
@@ -38,22 +29,34 @@ function generateMeme(currImg, topText, bottomText) {
   bottom.innerText = bottomText;
   btn.innerText = "Delete";
 
+  // change font size
+  top.style.fontSize = fontsize + "px";
+  bottom.style.fontSize = fontsize + "px";
+
   // append p to img & img to div -- load img
   outputdiv.append(newDiv);
   newDiv.append(newimg, top, bottom, btn);
 }
 
-form.addEventListener("submit", function(e) {
-  generateMeme(currImg, topText.value, bottomText.value);
-  topText.value = '';
-  bottomText.value = '';
+//deletes entire div containing the meme
+outputdiv.addEventListener("click", function(e) {
+  if (e.target.tagName === "BUTTON") { //tagName is a key in the target obj
+    e.target.parentElement.remove()
+  }
 });
 
-for (let btn of deleteBtns) {
-  btn.addEventListener("click", function(e) {
-    e.target.parentElement.remove()
-  });
-}
+//creates meme on submit & clears the inputs
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const currFile = currImg.files['0']; //this is an object
+  const url = URL.createObjectURL(currFile);
+  var currTextSize = textSize.value;
+  generateMeme(url, currTextSize, topText.value, bottomText.value);
 
+  topText.value = '';
+  bottomText.value = '';
+  currImg.value = '';
+});
 
-// DEBUG, how to save img WITH text?
+//currImg.addEventListener('change', function(e) {
+//})
